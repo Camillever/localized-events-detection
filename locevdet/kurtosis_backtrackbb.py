938a8bcfaef85247de54b82e04912aa56d340fee
Camillever <camille.huitorel@hotmail.fr>
2021-04-08 15:10:29 +0200

    New kurtosis from backtrackbb

---
+++ locevdet/kurtosis_backtrackbb.py
@@ -0,0 +1,14 @@
+""" Module to calculating kurtosis - inspired by 'BackTrackBB' algorithm""""
+import os
+
+def kurtosis_backtrackbb(win:float):
+
+    # YN1[n] = recursive_filter(y, CN_HP[n], CN_LP[n],
+    #                                   filter_npoles, rmem)
+    # YN1[n] /= filter_norm[n]
+
+    # CF1[n] = recursive_hos(YN1[n], CF_decay_constant,
+    #                                    hos_order, hos_sigma, rmem)
+
+    
+
