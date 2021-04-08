5684facba4fed9e99be7338736e5f6d17c683d65
Camillever <camille.huitorel@hotmail.fr>
2021-04-08 09:02:35 +0200

    Updated kurtosis application example

--- locevdet/examples/casse_riv_est/trainwaves_arrivals/kurtosis.py
+++ locevdet/examples/casse_riv_est/trainwaves_arrivals/kurtosis.py
@@ -8,7 +8,13 @@ mseeds_path = os.path.join("mseeds", "RESIF") # Seismograms
 sta_lta_save_path = os.path.join("sta_lta", "JSON") # Trainwaves dictionaries
 
 PRE_TRIGGER = 5 # in seconds
-POST_TRIGGER = 5 # in seconds
+POST_TRIGGER = 10 # in seconds
 
-kurtosis_for_all_seismograms(mseeds_path, sta_lta_save_path, PRE_TRIGGER, POST_TRIGGER)
+win = 0.2 # in seconds
+
+#Filter parameters
+FREQMIN=3
+FREQMAX=8
+
+kurtosis_for_all_seismograms(mseeds_path, sta_lta_save_path, PRE_TRIGGER, POST_TRIGGER, FREQMIN, FREQMAX, win)
 
