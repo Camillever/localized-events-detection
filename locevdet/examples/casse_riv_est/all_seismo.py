ea39ff4c66ddb8348c055acd93c3d76084eb26c5
Camillever <camille.huitorel@hotmail.fr>
2021-04-30 11:37:49 +0200

    New module to define all seismograms to use

---
+++ locevdet/examples/casse_riv_est/all_seismo.py
@@ -0,0 +1,29 @@
+""" Module to specify all the seismograms worthy to be use
+You can modify the conditions in this function"""
+
+import os
+
+from obspy import UTCDateTime
+
+from locevdet.utils import get_info_from_mseedname
+
+def list_seismograms(folder_path:str):
+    """  TODO"""
+
+    all_seismogram = [
+            filename for filename in os.listdir(folder_path)
+        if not filename.startswith('G_RER') 
+        if not filename.startswith('PF_TTR')  
+        ]
+        
+    # Remove specific station on some period
+    filenames = []
+    all_seismogram_reduced = []
+    for filename in all_seismogram:
+        starttime = UTCDateTime(get_info_from_mseedname(filename, 'starttime'))
+        if filename.startswith('PF_NSR') and starttime < UTCDateTime("2020-02-07T10:32:10"):
+            filenames.append(filename)
+            
+        else:
+            all_seismogram_reduced.append(filename)
+    return all_seismogram_reduced
