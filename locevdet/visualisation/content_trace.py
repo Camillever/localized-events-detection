ad1006a8f136da64108739c4c9df8e31ae178cd7
Camillever <camille.huitorel@hotmail.fr>
2021-06-01 09:15:50 +0200

    Updated content_trace all_seismogram will have to be given externally of the function (cf all_seismo)

--- locevdet/visualisation/content_trace.py
+++ locevdet/visualisation/content_trace.py
@@ -2,22 +2,22 @@
 
 import os
 
+from typing import List
+
 from obspy import read
 
-def content_traces(folder_in:str):
+def content_traces(all_seismogram:List[str], folder_in:str):
     """
     To visualize the content of all seismograms in the given folder
     Give few details of traces contained into these seismograms.
     
     Args:
+        all_seismogram : List of seismogram's filenames 
         folder_in: Directory path of MSEED seismograms
     Returns :
         print all the traces contained into each seismograms of the given folder
         and return the number of seismograms in this folder
     """
-    all_seismogram = [
-        filename for filename in os.listdir(folder_in)
-    ]
     for filename in all_seismogram:
         filepath = os.path.join(folder_in, filename)
         seismogram = read(filepath)
