51f031cf33aec4d713b0dca3176e2d982638ed02
Camillever <camille.huitorel@hotmail.fr>
2021-06-01 09:41:01 +0200

    Updated all_seismo Add docstring

--- locevdet/examples/casse_riv_est/all_seismo.py
+++ locevdet/examples/casse_riv_est/all_seismo.py
@@ -8,7 +8,13 @@ from obspy import UTCDateTime
 from locevdet.utils import get_info_from_mseedname
 
 def list_seismograms(folder_path:str):
-    """  TODO"""
+    """  Give the list of all seismograms from the given folder path.
+    Conditions could be given as excluding some stations or periods for some stations
+    Args:
+        folder_path : directory path of seismograms
+    Returns : 
+        List of seismogram's filenames
+    """
 
     all_seismogram = [
             filename for filename in os.listdir(folder_path)
