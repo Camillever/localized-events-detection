9b131a10765ae763681f977c45ae8c0a005a2ac7
Camillever <camille.huitorel@hotmail.fr>
2021-06-21 10:29:55 +0200

    Update download Add Pause Add ValueError if no instrument response saved

--- locevdet/download.py
+++ locevdet/download.py
@@ -1,6 +1,7 @@
 """ Module for waveforme preprocessing and downloading form a distant client. """
 
 import os
+import time
 from typing import List, Tuple, Callable
 
 import pandas as pd
@@ -12,7 +13,8 @@ from locevdet.utils import clean_utc_str, ref_duration_to_time_window, get_info_
 
 def download_from_stations(client:Client, network:str, stations:List[str],
         events_times:List[Tuple[float]], process_waveforms:Callable=lambda x:x,
-        location:str='*', channel:str='*', save_path:str=None, override:bool=False):
+        location:str='*', channel:str='*', save_path:str=None, override:bool=False, 
+        pause:int=0):
     """
     Download MSEED seismograms from the given client.
 
@@ -43,6 +45,7 @@ def download_from_stations(client:Client, network:str, stations:List[str],
     for i, station in enumerate(stations):
         network_txt = network + " " * (3 - len(network))
         desc_msg = f"Network {network_txt} - Station {i+1}/{len(stations)} ({station})"
+        all_seismograms_not_saved = []
         for start_time, end_time in tqdm(events_times, desc=desc_msg, total=len(events_times)):
             mseed_filename = '_'.join((
                 network, station,
@@ -52,12 +55,20 @@ def download_from_stations(client:Client, network:str, stations:List[str],
             mseed_save_path = os.path.join(save_path, mseed_filename)
 
             if override or not os.path.isfile(mseed_save_path):
-                waveforms = client.get_waveforms(
-                    network, station, location, channel,
-                    start_time, end_time, attach_response=True
-                )
-                waveforms = process_waveforms(waveforms)
-                waveforms.write(mseed_save_path, format='MSEED', encoding='FLOAT64')
+                if pause > 0:
+                    time.sleep(2)
+                try:
+                    waveforms = client.get_waveforms(
+                        network, station, location, channel,
+                        start_time, end_time, attach_response=True
+                    )
+                    waveforms = process_waveforms(waveforms)
+                    waveforms.write(mseed_save_path, format='MSEED', encoding='FLOAT64')
+                except ValueError:
+                    all_seismograms_not_saved.append((station, start_time, end_time))
+        if len(all_seismograms_not_saved)>0:
+            print(f"{len(all_seismograms_not_saved)} seismograms haven't been saved : \
+                {all_seismograms_not_saved}")
 
 def read_events_times_from_csv(csv_events_path:str, csv_delimiter:str=';',
         time_offset:Tuple[int]=(0, 0)) -> List[Tuple[float]]:
