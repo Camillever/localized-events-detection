30e3f6b181dc1635661f4216d74514a5ebd1e587
Camillever <camille.huitorel@hotmail.fr>
2021-06-25 14:16:07 +0200

    Update visualize script Add script visualisation on continuous seismograms

--- locevdet/examples/casse_riv_est/visualize.py
+++ locevdet/examples/casse_riv_est/visualize.py
@@ -2,6 +2,9 @@
 
 import os
 
+from obspy import UTCDateTime
+
+from locevdet.utils import get_info_from_mseedname
 from locevdet.visualisation.content_trace import content_traces
 from locevdet.visualisation.plots import captures_per_event
 from locevdet.examples.casse_riv_est.all_seismo import list_seismograms
@@ -27,26 +30,26 @@ CLIENT = 'RESIF'
 # ### Per event with spectrograms
 
 
-# VOLCANO-TECTONIC
-mseeds_volcanotectonic_path = \
-    os.path.join("seismograms", "01-02-2020_11-02-2020", "volcanotectonic")
-## Content of seismograms MSEED
-mseeds_path = os.path.join("seismograms", "01-02-2020_11-02-2020", "volcanotectonic")
-all_volcanotecto = [
-        filename for filename in os.listdir(mseeds_path)
-        if filename.startswith('PF_FRE') 
-        or filename.startswith('PF_HIM') 
-        or filename.startswith('G_RER')
-]
-print(all_volcanotecto)
-content_traces(all_seismogram=all_volcanotecto, folder_in=mseeds_volcanotectonic_path)
+# # VOLCANO-TECTONIC
+# mseeds_volcanotectonic_path = \
+#     os.path.join("seismograms", "01-02-2020_11-02-2020", "volcanotectonic")
+# ## Content of seismograms MSEED
+# mseeds_path = os.path.join("seismograms", "01-02-2020_11-02-2020", "volcanotectonic")
+# all_volcanotecto = [
+#         filename for filename in os.listdir(mseeds_path)
+#         if filename.startswith('PF_FRE') 
+#         or filename.startswith('PF_HIM') 
+#         or filename.startswith('G_RER')
+# ]
+# print(all_volcanotecto)
+# content_traces(all_seismogram=all_volcanotecto, folder_in=mseeds_volcanotectonic_path)
 # Plot
 ## Per Station
 
-## Per event
-save_fig_volcanotectonic_event_path = \
-    os.path.join("captures", "01-02-2020_11-02-2020", "volcanotectonic", "seismograms", "per_event")
-captures_per_event(mseeds_volcanotectonic_path, save_fig_path=save_fig_volcanotectonic_event_path)
+# ## Per event
+# save_fig_volcanotectonic_event_path = \
+#     os.path.join("captures", "01-02-2020_11-02-2020", "volcanotectonic", "seismograms", "per_event")
+# captures_per_event(mseeds_volcanotectonic_path, save_fig_path=save_fig_volcanotectonic_event_path)
 
 ### Per event with spectograms
 
@@ -66,3 +69,23 @@ captures_per_event(mseeds_volcanotectonic_path, save_fig_path=save_fig_volcanote
 #     os.path.join("captures", "01-02-2020_11-02-2020", "local_seisms", "seismograms", "per_event")
 # captures_per_event(mseeds_locals_path, save_fig_path=save_fig_locals_event_path)
 # ### Per event with spectograms
+
+
+# CONTINUOUS
+mseeds_path_continuous = os.path.join("seismograms","01-02-2020_11-02-2020","continuous")
+## Content of seismograms MSEED
+
+date_07fev = UTCDateTime("2020-02-07T00:00:00.00")
+before_07fev_seismograms =[ 
+    mseed for mseed in os.listdir(mseeds_path_continuous)
+    if get_info_from_mseedname(mseed)['starttime'] < date_07fev
+    if not mseed.startswith('PF_NSR') or not mseed.startswith('G_RER')
+    ]
+after_07fev_seismograms =[ 
+    mseed for mseed in os.listdir(mseeds_path_continuous)
+    if get_info_from_mseedname(mseed)['starttime'] > date_07fev
+    if not mseed.startswith('G_RER')
+    ]
+continuous_seismograms = [*before_07fev_seismograms, *after_07fev_seismograms]
+
+content_traces(all_seismogram=continuous_seismograms, folder_in=mseeds_path_continuous)
